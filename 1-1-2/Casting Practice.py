2ba00a722093e6328dabdd0d5dc2b9dc947b060d
13263 <jayden.buckley@vtsd.com>
2023-09-19 10:07:33 -0400

    test file

---
+++ 1-1-2/Casting Practice.py
@@ -0,0 +1,17 @@
+something = 1.2
+variable = 65
+umm = "uh"
+
+print("Initial values:")
+print("something:", something)
+print("variable:", variable)
+print("umm:", umm)
+print(str(something) + chr(variable) + str(umm))
+
+
+print("Ending Values)")
+print("something:", something)
+print("variable:", variable)
+print("umm:", umm)
+
+print("total:"
